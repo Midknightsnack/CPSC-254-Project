65ec029a0f7c31ef31d59fc6d2d3dd65af384b24
Bradley <bdippin12@csu.fullerton.edu>
2022-10-30 13:15:22 -0700

    created simple program

---
+++ base.cc
@@ -0,0 +1,29 @@
+#include <gtkmm.h>
+#include <iostream>
+using namespace std;
+
+void on_button_click();
+
+int main(int argc, char *argv[]){
+    auto app = Gtk::Application::create(argc, argv, "org.gtkmm.examples.base");
+    Gtk::Window window;
+    Gtk::Button button("Click here");
+
+    window.set_default_size(600, 360);
+    window.set_title("Burger Bistro");
+    window.set_position(Gtk::WIN_POS_CENTER);
+
+    button.signal_clicked().connect(
+        sigc::ptr_fun(&on_button_click)
+    );
+
+    button.show();
+    window.add(button);
+
+    return app->run(window);
+    return 0;
+}
+
+void on_button_click(){
+    cout << "Button clicked" << endl;
+}
