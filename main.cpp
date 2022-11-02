3717d15ed0b8544bc9f177f05224afa0c4c2b83d
Bradley <bdippin12@csu.fullerton.edu>
2022-11-02 14:51:04 -0700

    add executables

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int main(int argc, char *argv[]) {
     // Initialize window
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(window), "Burger Bistro");
-    gtk_window_set_default_size(GTK_WINDOW(window), 800, 360);
+    gtk_window_set_default_size(GTK_WINDOW(window), 1000, 800);
     gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
 
     fixed = gtk_fixed_new();
