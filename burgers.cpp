4c6576fb0a84db6bb885bc272e2c55ea1ce5f0b6
Bradley <bdippin12@csu.fullerton.edu>
2022-11-12 16:52:09 -0800

    added items to burger section

--- burgers.cpp
+++ burgers.cpp
@@ -28,8 +28,10 @@ static void load_css(void){
 
 int main(int argc, char *argv[]) {
     
-    GtkWidget *window, *fixed; 
-    GtkWidget *back_button, *cheeseburger_img, *cheeseburger_btn, *pay;
+    GtkWidget *window, *fixed, *back_button, *pay; 
+    //GtkWidget *cheeseburger_img;
+    GtkWidget *cheeseburger_btn, *cheeseburger_info, *ch_sand_btn, *ch_sand_info,
+              *pretzel_btn, *pretzel_info, *fish_btn, *fish_info;
 
     gtk_init(&argc, &argv);
     load_css();
@@ -48,18 +50,36 @@ int main(int argc, char *argv[]) {
     gtk_fixed_put(GTK_FIXED(fixed), back_button, 0, 0); 
     gtk_widget_set_size_request(back_button, 70, 30); 
 
+    // Cheeseburger Image/ Button
     // might have to resize the image manually tbh
-    //cheeseburger_img = gtk_image_new_from_file("/images/burgers/cheeseburger.jpg");
-    cheeseburger_btn = gtk_button_new_with_label("Cheeseburger");
+    // cheeseburger_img = gtk_image_new_from_file("/images/burgers/cheeseburger.jpg"); //wont load
+    // cheeseburger_btn = gtk_button_new_with_label("Cheeseburger");
+    cheeseburger_btn = gtk_button_new();
     gtk_widget_set_name(cheeseburger_btn, "cburg_btn");
-    //gtk_widget_set_name(cheeseburger_img, "cburg_img");
+    // gtk_widget_set_name(cheeseburger_img, "cburg_img");
 
     gtk_fixed_put(GTK_FIXED(fixed), cheeseburger_btn, 0, 50); 
     gtk_widget_set_size_request(cheeseburger_btn, 200, 200);
-    gtk_button_set_always_show_image(GTK_BUTTON(cheeseburger_btn), TRUE);
-    gtk_button_set_image (GTK_BUTTON(cheeseburger_btn), cheeseburger_img);
+    // gtk_button_set_always_show_image(GTK_BUTTON(cheeseburger_btn), TRUE);
+    // gtk_button_set_image (GTK_BUTTON(cheeseburger_btn), cheeseburger_img);
+    // g_signal_connect(cheeseburger_btn, "clicked", G_CALLBACK(burger_button_click), NULL);
 
-    g_signal_connect(cheeseburger_btn, "clicked", G_CALLBACK(burger_button_click), NULL);
+    cheeseburger_info = gtk_button_new_with_label("Cheeseburger: \nsesame buns, beef, onions," 
+                            " tomato, lettuce, cheese, bacon, ketchup");
+    gtk_fixed_put(GTK_FIXED(fixed), cheeseburger_info, 200, 50); 
+    gtk_widget_set_size_request(cheeseburger_info, 200, 200);
+
+
+    // Chicken Sandwich Image/ Button
+    ch_sand_btn = gtk_button_new();
+    gtk_widget_set_name(ch_sand_btn, "chicken_sand_btn");
+    gtk_fixed_put(GTK_FIXED(fixed), ch_sand_btn, 0, 300); 
+    gtk_widget_set_size_request(ch_sand_btn, 200, 200);
+
+    ch_sand_info = gtk_button_new_with_label("Chicken Sandwich: \nbrioche buns, fried chicken, " 
+                            " pickles, bistro sauce");
+    gtk_fixed_put(GTK_FIXED(fixed), ch_sand_info, 200, 300); 
+    gtk_widget_set_size_request(ch_sand_info, 200, 200);
 
     // Pay Online Button
     pay = gtk_button_new_with_label("Pay Online");
