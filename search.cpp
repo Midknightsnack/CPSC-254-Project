82fa6ab547898dad78eb3571d3a0fed2e5f8bc65
Bradley Diep <bdippin12@csu.fullerton.edu>
2022-11-21 21:18:31 -0800

    added GPL license

--- search.cpp
+++ search.cpp
@@ -1,3 +1,21 @@
+//     This file is part of Burger Bistro.
+
+//     Burger Bistro is a web application that allows users to interact with a restaurant website.
+//     Copyright (C) <2022>  <Bradley Diep>
+
+//     Burger Bistro is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+
+//     Burger Bistro is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+
+//     You should have received a copy of the GNU General Public License
+//     along with Burger Bistro. If not, see <https://www.gnu.org/licenses/>.
+
 #include <gtk/gtk.h>
 #include <string>
 #include <vector>
